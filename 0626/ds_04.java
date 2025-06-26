047e3a8c2e9ce0b2a2a7c9f47ebb88a6d8751cfd
ll05u <rita513513@gmail.com>
2025-06-26 23:52:36 +0800

    Update ds_04.java

--- 0626/ds_04.java
+++ 0626/ds_04.java
@@ -1,9 +1,7 @@
 public class ds_04 {
     public static void main(String[] args) {
-        // 建立並初始化一個含 5 個整數的陣列
         int[] numbers = {10, 20, 30, 40, 50};
 
-        // 輸出所有元素
         System.out.println("陣列的內容為：");
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("第 " + (i + 1) + " 個元素是: " + numbers[i]);
