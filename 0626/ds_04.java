948ba03b58f24a453a02ae0151ab4d4e22040e98
ll05u <rita513513@gmail.com>
2025-06-26 23:30:23 +0800

    Update ds_04.java
    新增作業

---
+++ 0626/ds_04.java
@@ -0,0 +1,25 @@
+public class ds_04 {
+    import java.util.Scanner;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("請輸入您的姓名：");
+        String name = scanner.nextLine();
+
+        System.out.print("請輸入您的年齡：");
+        int age = scanner.nextInt();
+        scanner.nextLine(); 
+
+        System.out.print("請輸入您居住的城市：");
+        String city = scanner.nextLine();
+
+        System.out.println("\n=== 使用者資訊 ===");
+        System.out.println("姓名：" + name);
+        System.out.println("年齡：" + age);
+        System.out.println("城市：" + city);
+
+        scanner.close();
+    }
+}
+
+}
