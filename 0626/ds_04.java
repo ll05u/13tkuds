88b17aeabc284ff4820c2a7312454c9793f87f5c
ll05u <rita513513@gmail.com>
2025-07-01 12:21:47 +0800

    Update ds_04.java

--- 0626/ds_04.java
+++ 0626/ds_04.java
@@ -1,8 +1,7 @@
 public class ds_04 {
     public static void main(String[] args) {
- 
         int[] numbers = {10, 20, 30, 40, 50};
-        int[] numbers = {10, 20, 30, 40, 50};
+        
         System.out.println("陣列的內容為：");
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("第 " + (i + 1) + " 個元素是: " + numbers[i]);
