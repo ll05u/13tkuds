362450a12c0fc79b15eca8bdaec1fd2e84bb068d
ll05u <rita513513@gmail.com>
2025-07-02 11:20:03 +0800

    add

---
+++ 0702/loopb.java
@@ -0,0 +1,12 @@
+public class loopb {
+    public static void main(String[] args) {
+        int x = 0;
+        int i = 1;
+
+        while (++i <= 100) {
+            x = x + 1;
+        }
+
+        System.out.println("x 的值為：" + x); 
+    }
+}
