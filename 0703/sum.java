bddca3961447ef95a7335b3a3cb23d54db3876d4
ll05u <rita513513@gmail.com>
2025-07-03 11:41:08 +0800

    add

--- 0703/sum.java
+++ 0703/sum.java
@@ -1,12 +1,9 @@
-public class sum1to100 {
+public class sum {
     public static void main(String[] args) {
-        int sum = 0;
+        int n = 100;
 
-        for (int i = 1; i <= 100; i++) {
-            sum += i;
-        }
+        int sum = n * (n + 1) / 2;
 
-       
-        System.out.println("1+2+3+4+5+.........+99+100: " + sum);
+        System.out.println("The sum from 1 to 100 is: " + sum);
     }
 }
