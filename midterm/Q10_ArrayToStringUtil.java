5507ae959e677760ae216b8d2cb2e3c4fa96259e
ll05u <rita513513@gmail.com>
2025-07-16 11:14:56 +0800

    Merge branch 'main' of https://github.com/ll05u/13tkuds

---
+++ midterm/Q10_ArrayToStringUtil.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Q10_ArrayToStringUtil {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] arr = new int[n];
+        String[] input = scanner.nextLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        System.out.println(arrayToString(arr));
+        scanner.close();
+    }
+
+    public static String arrayToString(int[] arr) {
+        if (arr == null) {
+            return "null";
+        }
+        if (arr.length == 0) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i < arr.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
